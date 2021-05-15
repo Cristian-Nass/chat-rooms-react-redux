b68ff3b61e7d91dfe1ae04e0e24d40a662c2e4ff
Cristian <cristian.tirdad@gmail.com>
2021-05-15 13:56:51 +0200

    implemented redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import {increment, decrement} from "./store/CounterSlice";
+
 import './App.css';
 
 function App() {
+
+  // @ts-ignore
+  const count = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      Chat Rooms
+      <h1> The count is: {count}</h1>
+      <button onClick={() => dispatch(increment())}> Increment </button>
+      <button onClick={() => dispatch(decrement())}> Decrement </button>
     </div>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
+import counterSlice from "./CounterSlice";
+  
+  const reducer = ({
+    counter: counterSlice
+  });
+  
+  const store = configureStore({
+    reducer //reducer: reducer
+  });
+  
+  export default store;
